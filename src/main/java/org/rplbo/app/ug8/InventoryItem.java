ddd0c248b5542ab188d4e4733157d0d4eb2800c5
SamuelN1508 <samuelnatanael08@gmail.com>
2026-04-20 21:02:50 +0700

    123

--- src/main/java/org/rplbo/app/ug8/InventoryItem.java
+++ src/main/java/org/rplbo/app/ug8/InventoryItem.java
@@ -13,13 +13,35 @@ public class InventoryItem {
         this.totalStock = totalStock;
     }
 
-    // Getter dan Setter
-    public String getItemName() { return itemName; }
-    public void setItemName(String itemName) { this.itemName = itemName; }
-    public int getAcquired() { return acquired; }
-    public void setAcquired(int acquired) { this.acquired = acquired; }
-    public int getUsed() { return used; }
-    public void setUsed(int used) { this.used = used; }
-    public int getTotalStock() { return totalStock; }
-    public void setTotalStock(int totalStock) { this.totalStock = totalStock; }
+    public String getItemName() {
+        return itemName;
+    }
+
+    public void setItemName(String itemName) {
+        this.itemName = itemName;
+    }
+
+    public int getAcquired() {
+        return acquired;
+    }
+
+    public void setAcquired(int acquired) {
+        this.acquired = acquired;
+    }
+
+    public int getUsed() {
+        return used;
+    }
+
+    public void setUsed(int used) {
+        this.used = used;
+    }
+
+    public int getTotalStock() {
+        return totalStock;
+    }
+
+    public void setTotalStock(int totalStock) {
+        this.totalStock = totalStock;
+    }
 }
